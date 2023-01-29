cf2c8e1edfd3168e26937bc58e0a14c23c9c7332
Daniel Segall <segalld@gmail.com>
2023-01-29 14:46:22 +0000

    Initial Commit

---
+++ FiniteStateMachine/FiniteStateMachine.h
@@ -0,0 +1,18 @@
+//
+//  FiniteStateMachine.h
+//  FiniteStateMachine
+//
+//  Created by Daniel Segall on 29/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FiniteStateMachine.
+FOUNDATION_EXPORT double FiniteStateMachineVersionNumber;
+
+//! Project version string for FiniteStateMachine.
+FOUNDATION_EXPORT const unsigned char FiniteStateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FiniteStateMachine/PublicHeader.h>
+
+
