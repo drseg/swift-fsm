40c7f6efe5e02b973cd16ecbe0b60b5e728ca43e
Daniel Segall <segalld@gmail.com>
2023-04-27 13:02:33 +0100

    Test nested override

--- Tests/SwiftFSMTests/Syntax/BlockSentenceComponentTests.swift
+++ Tests/SwiftFSMTests/Syntax/BlockSentenceComponentTests.swift
@@ -1122,13 +1122,37 @@ class ThenBlockTests: DefaultIOBlockTests {
 }
 
 class OverrideBlockTests: DefaultIOBlockTests {
-    func test() {
+    func testOverride() {
         let o1 = override { mwtaBlock }
         let o2 = Override { mwtaBlock }
         
         let nodes = [o1, o2].flattened.nodes.map { $0 as! OverridableNode }
         nodes.forEach {
             XCTAssert($0.isOverride)
+        }
+    }
+    
+    func testNestedOverride() {
+        let d = define(1) {
+            override {
+                mwtaBlock
+            }
+            mwtaBlock
+        }
+        
+        let g = d.node.rest.first as! GivenNode
+        
+        XCTAssertEqual(4, g.rest.count)
+        
+        let overridden = g.rest.prefix(2).map { $0 as! OverridableNode }
+        let notOverridden = g.rest.suffix(2).map { $0 as! OverridableNode }
+        
+        overridden.forEach {
+            XCTAssertTrue($0.isOverride)
+        }
+        
+        notOverridden.forEach {
+            XCTAssertFalse($0.isOverride)
         }
     }
 }
