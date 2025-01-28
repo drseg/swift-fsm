b847f579dd5890c67ad3c952399819704dc0122d
Daniel Segall <segalld@gmail.com>
2025-01-28 14:57:58 +0000

    Experimental fix for CI bug

--- Sources/SwiftFSM/Syntax/Functions/overrides.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public extension SyntaxBuilder {
-    func overriding(
-        @Internal.MWTABuilder _ block: () -> [MWTA]
-    ) -> [MWTA] {
-        Syntax.Override().callAsFunction(block)
-    }
-}
