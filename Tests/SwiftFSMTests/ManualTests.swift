f9f8308db73c12183542f95b6c6ba0d1d9a35365
Daniel Segall <segalld@gmail.com>
2025-02-08 18:55:55 +0000

    Add manual compliation tests for things that shouldn’t compile

---
+++ Tests/SwiftFSMTests/ManualTests.swift
@@ -0,0 +1,135 @@
+#if(false)
+@testable import SwiftFSM
+
+class ManualTests: ExpandedSyntaxBuilder {
+    typealias State = Int
+    typealias Event = Int
+    
+    enum A: Predicate { case x }
+    enum B: Predicate { case x }
+    
+    func function() { }
+    
+    func orTypesMustBeTheSame() {
+        _ = matching(A.x, or: B.x)
+    }
+    
+    func cannotChainWhens1() {
+        define(1) {
+            when(1) {
+                when(1) { }
+            }
+        }
+    }
+    
+    func cannotChainWhens2() {
+        define(1) {
+            when(1) {
+                when(1) | then()
+            }
+        }
+    }
+    
+    func cannotChainWhens3() {
+        define(1) {
+            when(1) {
+                matching(A.x) | when(1) | function
+            }
+        }
+    }
+    
+    func cannotChainWhens4() {
+        define(1) {
+            when(1) {
+                matching(A.x) {
+                    when(1) { }
+                }
+            }
+        }
+    }
+    
+    func cannotChainWhens5() {
+        define(1) {
+            when(1) {
+                matching(A.x) {
+                    when(1) | then()
+                }
+            }
+        }
+    }
+    
+    func cannotChainThens1() {
+        define(1) {
+            then(1) {
+                then(1) { }
+            }
+        }
+    }
+    
+    func cannotChainThens2() {
+        define(1) {
+            then(1) {
+                then(1) | then()
+            }
+        }
+    }
+    
+    func cannotChainThens3() {
+        define(1) {
+            then(1) {
+                matching(A.x) | then(1) | function
+            }
+        }
+    }
+    
+    func cannotChainThens4() {
+        define(1) {
+            then(1) {
+                matching(A.x) {
+                    then(1) { }
+                }
+            }
+        }
+    }
+    
+    func cannotChainThens5() {
+        define(1) {
+            then(1) {
+                matching(A.x) {
+                    then(1) | then()
+                }
+            }
+        }
+    }
+    
+    func cannotWhenMultipleThen() {
+        define(1) {
+            when(2) {
+                then(.unlocked)
+                then(1)
+                then(.unlocked) | function
+                then(1)         | function
+            }
+        }
+    }
+    
+    func invalidBlocksAfterPipes1() {
+        define(1) {
+            when(1)       | then(1) { }
+        }
+    }
+    
+    func invalidBlocksAfterPipes2() {
+        define(1) {
+            when(1)       | then(1) | actions(function) { }
+        }
+    }
+    
+    func invalidBlocksAfterPipes3() {
+        define(1) {
+            matching(A.x) | when(1) { }
+        }
+    }
+}
+
+#endif
